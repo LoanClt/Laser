ca1c1dea010d246327d0e0f69870295b97053e13
Looooan <51877319+Looooan@users.noreply.github.com>
2023-03-26 15:55:22 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,49 +1,58 @@
 import matplotlib.pyplot as plt
+
 import math as m
+
 import numpy as np
 
-A21 = 3*10**(6) #s-1
-sigmaP = 9.3*10**(-24) #m²
-sigma = 3*10**(-23)
-longueurOnde = 514*10**(-9) #m
-c = 3*10**8 #m.s-1
-h = 6.62607015*10**(-34)
-frequence = c/longueurOnde #s-1
-rayon = 50*10**(-6) #m
-surface = m.pi*rayon**2
+A21 = 3 * 10 ** (6)  # s-1
+
+sigmaP = 9.3 * 10 ** (-24)  # m²
+
+sigma = 3 * 10 ** (-23)
+
+longueurOnde = 514 * 10 ** (-9)  # m
+
+c = 3 * 10 ** 8  # m.s-1
+
+h = 6.62607015 * 10 ** (-34)
+
+frequence = c / longueurOnde  # s-1
+
+rayon = (50 / 2) * 10 ** (-6)  # m
+
+surface = m.pi * rayon ** 2  # m²
+
 
 def deltaNsurN(I, puissancePompage):
     intensitePompage = puissancePompage / (surface * h * frequence)
-    return sigmaP*intensitePompage/(A21 + sigma*I + sigmaP*intensitePompage)
+
+    return sigmaP * intensitePompage / (A21 + sigma * I + sigmaP * intensitePompage)
+
 
 Y = []
+
 Z = []
+
 W = []
 
-"""
-X = np.linspace(1, m.log(10**50), 100000)
-for i in range(len(X)):
-    X[i] = m.log(X[i])
-    Y.append(deltaNsurN(X[i], 1))
-    Z.append(deltaNsurN(X[i], 10))
-    W.append(deltaNsurN(X[i], 100))
+X = np.logspace(1, 50, 10000, base=10)
 
-"""
-X = []
-N = 500000
-for i in range(1, N):
-    I = 10**i
-    X.append(m.log(I))
-    Y.append(deltaNsurN(m.log(I), 1))
-    Z.append(deltaNsurN(m.log(I), 10))
-    W.append(deltaNsurN(m.log(I), 100))
+Y = deltaNsurN(X, 1)
 
+Z = deltaNsurN(X, 10)
 
+W = deltaNsurN(X, 100)
 
-plt.plot(X, Y, label="P = 1W")
-plt.plot(X, Z, label="P = 10W")
-plt.plot(X, W, label="P = 100W")
+plt.plot(np.log(X), Y, label="P = 1W")
+
+plt.plot(np.log(X), Z, label="P = 10W")
+
+plt.plot(np.log(X), W, label="P = 100W")
+
 plt.xlabel("log(I)")
+
 plt.legend()
-plt.ylabel("DeltaN/N")
-plt.show()
+
+plt.ylabel("DeltaN/Nt")
+
+plt.xlim(60, 75)
